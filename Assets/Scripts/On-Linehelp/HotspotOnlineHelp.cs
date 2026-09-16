efd34fe0d1e5605e4baa0aec955ce07169b6a3bc
Gsantos2001 <ice20390204@uniwa.gr>
2026-09-16 22:21:18 +0300

    On-Line Help test

---
+++ Assets/Scripts/On-Linehelp/HotspotOnlineHelp.cs
@@ -0,0 +1,10 @@
+
+using UnityEngine;
+
+public class HotspotOnlineHelp : MonoBehaviour
+{
+    [Header("Hotspot Online Help")]
+    [TextArea(3, 10)]
+    public string helpMessage =
+        "Hotspot instructions...";
+}
